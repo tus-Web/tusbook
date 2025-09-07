a0d84dd57bfbfedf77c51566f9088c94b28d68f7
Cota2006 <lecture_singers_0u@icloud.com>
2025-09-07 21:18:29 +0900

    メニューを選べるようにしました

---
+++ src/app/id/page.tsx
@@ -0,0 +1,45 @@
+"use client";
+import styles from "./page.module.css";
+import Link from "next/link";
+import { useParams } from "next/navigation";
+
+
+const subjects = {
+  udon: { h1: "学食レビュー", h2: "うどん" },
+  shio_ramen: { h1: "学食レビュー", h2: "塩ラーメン" },
+  omuraisu: { h1: "学食レビュー", h2: "オムライス" },
+};
+
+export default function ReviewPage() {
+  const params = useParams(); //URLのidを取得するらしい
+  const subject = subjects[params.id as keyof typeof subjects]; //URLのidに対応する情報を取得するらしい
+
+  if (!subject) {
+    return <div>
+        <p>該当するものが見つかりませんでした。</p>
+        <Link href="/">戻る</Link>
+    </div>;
+  }
+
+  return (
+     <div className={styles.main}>
+      <h1>{subject.h1}</h1>
+      <h2>{subject.h2}</h2>
+
+ {/* 回答を記録するときはformタグを使います */}
+ {/*データを保存したいときはsupabaseを使います*/}
+      <form action="" className={styles.form}>
+        <textarea
+          name="content" // addReview関数で取り出すための名前
+          rows={4}
+          placeholder="レビューを記入してください..."
+          required
+        />
+        <button type="submit" className={styles.button}>
+          レビューを投稿
+        </button>
+        </form>
+        <Link href="/">戻る</Link>
+    </div>
+  )
+  }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,23 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client";
+import Link from "next/link";
 
-export default function Home() {
-  return (
-     <div className={styles.main}>
-      <h1>授業レビュー</h1>
-      <h2>数学演習１</h2>
-
- {/* 回答を記録するときはformタグを使います */}
- {/*データを保存したいときはsupabaseを使います*/}
-      <form action="" className={styles.form}>
-        <textarea
-          name="content" // addReview関数で取り出すための名前
-          rows={4}
-          placeholder="レビューを記入してください..."
-          required
-        />
-        <button type="submit" className={styles.button}>
-          レビューを投稿
-        </button>
-  
-        </form>
-    </div>
-  )
-  }
+export default function Menu() {
+    const subjects = [
+        { id: "udon", name: "うどん" },
+        { id: "shio_ramen", name: "塩ラーメン" },
+        { id: "omuraisu", name: "オムライス" },
+    ];
+    
+    return (
+        <main>
+            <h1>学食レビュー</h1>
+            <ul>
+                {subjects.map((subject) => (
+                    <li key={subject.id}>
+                        <Link href={`/${subject.id}`}>{subject.name}</Link>
+                    </li>
+                ))}
+            </ul>
+        </main>
+    );
+}
